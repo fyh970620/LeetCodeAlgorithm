4e854e4e316d3bd0988659b9b8724069f6e422a2
fengyhhhhhh <39217063+fyh970620@users.noreply.github.com>
2022-05-17 15:31:23 +0800

    Add files via upload
    Finally monotonous stack exercise

--- CountProject/src/cn/fyihan/单调栈/LeetCode962最大宽坡度.java
+++ CountProject/src/cn/fyihan/单调栈/LeetCode962最大宽坡度.java
@@ -1,7 +1,34 @@
 package cn.fyihan.单调栈;
 
+
 import java.util.Stack;
 
 public class LeetCode962最大宽坡度 {
-    
+
+    public static int maxWidthRamp(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+        // 递减序列
+        for(int i=0; i<nums.length; i++) {
+            if(stack.isEmpty()) {
+                stack.push(i);
+                continue;
+            }
+            if(!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+        int maxSlop = 0;
+        // 从尾部遍历数组
+        for(int i=nums.length-1; i>=0; i--) {
+            while(!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
+                int slopStartIndex = stack.pop();
+                maxSlop = Math.max(maxSlop, i-slopStartIndex);
+            }
+        }
+        return maxSlop;
+    }
+
+    public static void main(String[] args) {
+        maxWidthRamp(new int[]{9,8,1,0,1,9,4,0,4,1});
+    }
 }
